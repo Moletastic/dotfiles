58a87445e60d597c16601fdf34e67b30632bce36
Jake <jacob.romero@transvip.cl>
2024-06-02 13:00:51 -0400

    feat: updates dwm config

--- dwm/.local/share/dwm/autostart.sh
+++ dwm/.local/share/dwm/autostart.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec $HOME/.screenlayout/default.sh & exec /usr/local/bin/slstatus & exec $HOME/.fehbg & exec picom --config $HOME/.config/picom/picom.conf & redshift
+exec $HOME/.screenlayout/default.sh & exec /usr/local/bin/slstatus & exec $HOME/.fehbg & redshift & dunst
